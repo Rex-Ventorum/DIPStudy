4759aa0a217e87ce3e4f07098085a0ac5815bdf1
Brandon <Brandon@NubCenter.attlocal.net>
2017-09-25 12:47:01 -0500

    Added Methods to FlorecentBulb

--- src/LightSwitchInterface/FlorecentBulb.java
+++ src/LightSwitchInterface/FlorecentBulb.java
@@ -1,13 +1,18 @@
 package LightSwitchInterface;
 public class FlorecentBulb {
     private boolean isOn;
-    private WattageType wattage;
+    private Wattage wattage;
     
-    public FlorecentBulb(int wattage){
+    public FlorecentBulb(Wattage wattage){
         setWattage(wattage);
     }
     
-    public void setWattage(int wattage){
-        if(wattage)
+    public void setWattage(Wattage wattage){
+        if(wattage == null) throw new IllegalArgumentException("Wattage May Not Be Null");
+        this.wattage = wattage;
     }
+    
+    public void turnOn(){isOn = true;}
+    public void turnOff(){isOn = false;}
+    public boolean isOn(){return isOn;}
 }
--- src/LightSwitchInterface/Wattage.java
+++ src/LightSwitchInterface/Wattage.java
@@ -1,6 +1,6 @@
 package LightSwitchInterface;
 
-public enum WattageType {
-     WATT_8, WATT_12,WATT_14,WATT_20,WATT_10,WATT_15,WATT_20, WATT_23,
+public enum Wattage {
+     WATT_8, WATT_12,WATT_14,WATT_20,WATT_10,WATT_15, WATT_23,
      WATT_28,WATT_43,WATT_53,WATT_72,WATT_40,WATT_60,WATT_75,WATT_100;
 }
